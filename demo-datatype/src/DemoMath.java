562093eb1e74739152d98872e264891beba554a2
Henry_WU <chakpingwu@gmail.com>
2025-12-02 14:41:46 +0800

    add print f1 & s20

--- demo-datatype/src/DemoMath.java
+++ demo-datatype/src/DemoMath.java
@@ -24,22 +24,24 @@ public class DemoMath {
     System.out.println(a); // 17
 
     //Step 1: int + double
-    //Step 2: Assign double value into double box
+    
     int b = 8;
-
+    //Step 2: Assign double value into double box
     System.out.println(b+ 3.5d);
-    double r = b + 3.5d;
+    //double r = b + 3.5d;
 
     //! Operation between higher level type and lower level type -> Result: higher level type
     long k = 3 * 100L;
     //int t = 3* 100L; // int * long -> long, cannot assign to int box
     //! byte -> short -> int -> long -> float -> double
     float f1 = 10L;
+    System.out.println(f1);
     byte b10 = 9;
     short s10 = 9;
     // ! Special Case: byte + short ->int
     // Short s20 = b10 + s10;
     int s20 = b10 + s10; //OK
+    System.out.println(s20);
 
     //char -> int
 
