029d64b6c3764bbb3aa1b05f8472fb76bd965936
Henry_WU <chakpingwu@gmail.com>
2025-12-06 01:57:06 +0800

    upload demoPractise

---
+++ demo-helloWorld/src/demoPractise.java
@@ -0,0 +1,24 @@
+public class demoPractise {
+  
+  public static void main(String[] args) {
+    
+    char zero = 'F';
+    char zero2 = 'A';
+    
+    if(zero < zero2){
+
+      System.out.println(zero + " come before " + zero2);
+    
+    }
+
+    else if( zero > zero2 ){
+
+      System.out.println(zero + " come after " + zero2);
+
+    }else{
+
+      System.out.println(zero + " is equal to " + zero2);
+    }
+  
+  }
+}
