895498d3d18b49f3a9cd1738ebf5044b4b5915cd
Henry_WU <chakpingwu@gmail.com>
2025-12-23 13:15:01 +0800

    Ex7

--- demo-data-structure/src/Exercise/DataStructureExercise.java
+++ demo-data-structure/src/Exercise/DataStructureExercise.java
@@ -2,6 +2,7 @@ package Exercise;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class DataStructureExercise {
   // Exercise 1: Array List Basic Operations
@@ -67,6 +68,106 @@ public class DataStructureExercise {
 
     System.out.println("Exercise 3: Remove Duplicates");
     System.out.println("Ex3. Remove duplicates from the list " + exQuest3);
+    System.out.println();
+
+    // Exercise 4: HashSet Basic Operations
+
+    // 4a. Create a HashSet of strings to store country names: "USA", "India", "China", "Japan".
+    // 4b. Add "Canada" to the set.
+    // 4c. Add "India" again. Print the result.
+    // 4d. Print all elements in the set.
+
+    System.out.println("Exercise 4: HashSet Basic Operations");
+    HashSet<String> exQuest4 = new HashSet<>();
+    exQuest4.add("USA");
+    exQuest4.add("India");
+    exQuest4.add("China");
+    exQuest4.add("Japan");
+    System.out.println("Ex4. The original list: " + exQuest4);
+    System.out.println("Ex4. Add \"Canada\" to the set: ");
+    exQuest4.add("Canada");
+    System.out.println(exQuest4);
+    System.out.println("Ex4. Add \"India\" again.");
+    exQuest4.add("India");
+    System.out.println(exQuest4);
+    System.out.println();
+
+    // Exercise 5: Check the numbers
+
+    // 5a. Create a HashSet of numbers: 1.1, 2.2, 3.3, 4.4, 5.5
+    // 5b. Check if the set contains the number 3.3
+    // 5c. Remove the number 2.2 from the set.
+    // 5d. Print the size of the set.
+
+    HashSet<Double> exQuest5 = new HashSet<>();
+    exQuest5.add(1.1);
+    exQuest5.add(2.2);
+    exQuest5.add(3.3);
+    exQuest5.add(4.4);
+    exQuest5.add(5.5);
+
+    System.out.println("Exercise 5: Check the numbers");
+    System.out.println("Ex5. Check the set contains the number 3.3: "
+        + exQuest5.contains((Double) 3.3));
+
+    if (exQuest5.contains((Double) 3.3)) {
+
+      System.out.println("Ex5. Remove the number 2.2 from the set: "
+          + exQuest5.remove((Double) 2.2));
+
+    }
+    System.out.println(exQuest5);
+    System.out.println();
+
+
+    // Exercise 6: Intersection of Sets
+
+    // 6a. Create two HashSets:
+    // 6b. Set 1: 10, 20, 30, 40
+    // 6c. Set 2: 30, 40, 50, 60
+    // 6d. Find the common numbers of the two sets.
+    // 6e. Print the resulting set.
+
+    HashSet<Integer> exQuest6Set1 = new HashSet<>();
+    exQuest6Set1.add(10);
+    exQuest6Set1.add(20);
+    exQuest6Set1.add(30);
+    exQuest6Set1.add(40);
+
+    HashSet<Integer> exQuest6Set2 = new HashSet<>();
+    exQuest6Set2.add(30);
+    exQuest6Set2.add(40);
+    exQuest6Set2.add(50);
+    exQuest6Set2.add(60);
+
+    System.out.println("Exercise 6: Intersection of Sets");
+    System.out.println("Ex6. The original set 1 list: " + exQuest6Set1);
+    System.out.println("Ex6. The original set 2 list: " + exQuest6Set2);
+    exQuest6Set1.retainAll(exQuest6Set2);
+    System.out.println("Ex6. The set1: " + exQuest6Set1);
+    exQuest6Set2.retainAll(exQuest6Set1);
+    System.out.println("Ex6. The set2: " + exQuest6Set2);
+    System.out.println();
+
+    // Exercise 7: Convert HashSet to ArrayList
+
+    // 7a. Create a HashSet with the following String: "Cherry", "Steve", "Chole", "Jenny", "Vicky".
+    // 7b. Convert the HashSet to an ArrayList.
+    // 7c. Print the converted list.
+
+    System.out.println("Exercise 7: Convert HashSet to ArrayList");
+    HashSet<String> exQuest7 = new HashSet<>();
+    exQuest7.add("Cherry");
+    exQuest7.add("Steve");
+    exQuest7.add("Chole");
+    exQuest7.add("Jenny");
+    exQuest7.add("Vicky");
+
+    System.out.println("Ex7. This is HashSet: " + exQuest7);
+    ArrayList<String> arrayList_exQuest7 = new ArrayList<>(exQuest7);
+    System.out.println("Ex7. Convert to an ArrayList: " + arrayList_exQuest7);
+    System.out.println("Is the ArrayList is HashSet? "+arrayList_exQuest7.equals(exQuest7));
+    System.out.println();
 
   }
 
